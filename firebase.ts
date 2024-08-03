1259a766b0ab27fddb92dd0f7bee799017a76c84
Jeevan Babu <jeevanbabu190@gmail.com>
2024-08-03 20:46:51 +0530

    fix error

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,5 @@
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,7 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+
 
 
 export default app;
